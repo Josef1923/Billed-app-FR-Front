4c093945e24302672ddf7fe70a1a1ec8b4e12bd7
Josef1923 <schererjos01@gmail.com>
2024-11-04 14:15:30 +0100

    Passage test bills en vert (BillsUI.js; L 22 à 31)

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -17,14 +17,22 @@ const row = (bill) => {
       </td>
     </tr>
     `)
-  }
-
-const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
 }
 
+const rows = (data) => {
+  // Vérifie qu'il y ait bien des datas
+  if (!data || !data.length) return "";
+
+  // Trier les bbills du plus récent au moins récent
+  const sortedData = data.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+  //retourne les datas triées
+  return sortedData.map(bill => row(bill)).join("");
+};
+
+
 export default ({ data: bills, loading, error }) => {
-  
+
   const modal = () => (`
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
@@ -47,7 +55,7 @@ export default ({ data: bills, loading, error }) => {
   } else if (error) {
     return ErrorPage(error)
   }
-  
+
   return (`
     <div class='layout'>
       ${VerticalLayout(120)}
